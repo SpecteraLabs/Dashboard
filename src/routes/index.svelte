fffb2e41ff28164846a64b41d45914f57fb26694
UndiedGamer <hemanthchakka2007@gmail.com>
2021-12-19 13:19:41 +0530

    feat: init

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<Navbar />
+<script lang="ts">
+	import Footer from '../components/_footer.svelte';
+	import Navbar from '../components/_navbar.svelte';
+	import '../styles/tailwind-output.css';
+</script>
+<Footer />
